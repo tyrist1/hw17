da0e4e67b63b0f3691e3691690bbd72a1ba3badf
Andrei <tyrist84@mail.ru>
2022-02-24 19:39:48 +0300

    запуск

--- app.py
+++ app.py
@@ -24,18 +24,49 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+class MovieSchema(Schema):
+    id =fields.Int()
+    title = fields.Str()
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+class DirectorSchema(Schema):
+    id =fields.Int()
+    name = fields.Str()
 
 class Genre(db.Model):
     __tablename__ = 'genre'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+class GenreSchema(Schema):
+    id =fields.Int()
+    name = fields.Str()
 
+movie_schema=MovieSchema()
+movies_schema=MovieSchema(many=True)
+
+api = Api(app)
+
+movie_ns=api.namespaces('')
+director_ns=api.namespaces('')
+genre_ns=api.namespaces('')
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        res = Movie.query
+        if director_id is not None:
+            res = res.filter(Movie.director_id == director_id)
+        if genre_id is not None:
+            res = res.filter(Movie.genre_id == genre_id)
+        result = res.all()
+        return movies_schema.dump(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
